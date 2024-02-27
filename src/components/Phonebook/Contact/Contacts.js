cb394777da13eac3fc721e683938cae84271808d
RodionKipitok <kucernukrabota@gmail.com>
2024-02-27 21:30:52 +0200

    update contact

--- src/components/Phonebook/Contact/Contacts.js
+++ src/components/Phonebook/Contact/Contacts.js
@@ -25,31 +25,3 @@ function Contact({ contacts, fucnDelete }) {
 }
 
 export default Contact;
-
-// class Contacts extends Component {
-// 	const contacts =
-//   render() {
-//     return (
-//       <>
-//         <h2 className="titleContact">Contacts</h2>
-//         <ul className="list">
-//           {[].map(contact => (
-//             <li className="contactItem" key={contact.id}>
-//               {contact.name} {contact.number}{' '}
-//               <button
-//                 className="btnContact"
-//                 onClick={() => {
-//                   console.log(contact.id);
-//                 }}
-//               >
-//                 Delete
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       </>
-//     );
-//   }
-// }
-
-// export default Contacts;
